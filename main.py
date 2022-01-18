b5419935b9efdf34a8d1a866ee85e485a8fedfa5
Ted <ted.pinkerton@mail.utoronto.ca>
2022-01-17 20:06:25 -0500

    Main file exists
    For now there is only a todo in main but in the future it will manage the other files.

---
+++ main.py
@@ -0,0 +1,2 @@
+# @todo Figure out the game-player interface
+# @body I need the interface to be simple enough to use. Ideally I can just pass a list to each player but I'm not sure if that is reasonable with large and varied maps
